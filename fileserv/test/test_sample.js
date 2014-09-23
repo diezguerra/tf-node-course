bc90231fab5a627e05bb28fdce2296f4cfa098e3
Saul Diez-Guerra <saul@diezguerra.com>
2014-09-23 19:46:20 -0400

    file server unit2 assignment 3

---
+++ fileserv/test/test_sample.js
@@ -0,0 +1,9 @@
+var should = require("chai").should();
+var expect = require("chai").expect;
+
+describe('Sample', function(){
+
+        it('should pass', function(){
+            expect(1).to.equal(1);
+        });
+});
